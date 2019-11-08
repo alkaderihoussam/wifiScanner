be79ac5a81abd2154a1c9a84c0575f7e3e1d3f62
Houssam-Eddine AL KADERI <alkaderihoussam@gmail.com>
2019-11-08 15:21:22 +0100

    first commit

---
+++ app/src/main/java/com/example/wifiscarnner/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.wifiscarnner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void showWifiInfos(View view) {
+
+        TextView ssidField = (TextView) findViewById(R.id.ssid);
+        TextView netidField = (TextView) findViewById(R.id.netid);
+        TextView debitField = (TextView) findViewById(R.id.debit);
+        TextView freqField = (TextView) findViewById(R.id.freq);
+        TextView macField = (TextView) findViewById(R.id.mac);
+        TextView rssiField = (TextView) findViewById(R.id.rssi);
+
+        WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        //wifiManager.setWifiEnabled(true);
+
+        WifiInfo currentWifiNetworkInfo = wifiManager.getConnectionInfo();
+
+        ssidField.setText(currentWifiNetworkInfo.getSSID());
+        netidField.setText(Integer.toString(currentWifiNetworkInfo.getNetworkId()));
+        debitField.setText(Integer.toString(currentWifiNetworkInfo.getLinkSpeed()));
+        freqField.setText(Integer.toString(currentWifiNetworkInfo.getFrequency()));
+        macField.setText(currentWifiNetworkInfo.getMacAddress());
+        rssiField.setText(Integer.toString(currentWifiNetworkInfo.getRssi()));
+
+    }
+}
